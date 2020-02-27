25e4037623ddef23840b6bc31986047dbfa46306
isturm <iavor.sturm@uni-hamburg.de>
2020-02-27 14:46:38 +0100

    Candidat Bug Fix and Feature Package [master-v.1-b5196] on Friday, Februar 21, 2020

---
+++ tomcat-7.0.62/webapps/lecture2go-portlet/WEB-INF/src/de/uhh/l2g/plugins/util/OaiPmhManager.java
@@ -0,0 +1,345 @@
+/*******************************************************************************
+ * Lecture2Go-License
+ * The Lecture2Go software is based on the liferay portal 6.2-ga6
+ * <http://www.liferay.com>
+ * 
+ * Lecture2Go <http://lecture2go.uni-hamburg.de> is an open source
+ * platform for media management and distribution. Our goal is to
+ * support the free access to knowledge because this is a component
+ * of each democratic society. The open source software is aimed at
+ * academic institutions and has to strengthen the blended learning.
+ * 
+ * All Lecture2Go plugins are continuously being developed and improved.
+ * For more details please visit <http://lecture2go-open-source.rrz.uni-hamburg.de>
+ * 
+ * Copyright (c) 2013 - present University of Hamburg / Computer and Data Center (RRZ)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * Third Party Software
+ * 
+ * Lecture2Go uses third-party libraries which may be distributed under different licenses to 
+ * the above (but are compatible with the used GPL license). Informations about these licenses 
+ * and copyright informations are mostly detailed in the library source code or jars themselves. 
+ * You must agree to the terms of these licenses, in addition to  the above Lecture2Go 
+ * source code license, in order to use this software. 
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * Apache 2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and limitations under the License.
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * BSD
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software without 
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * Eclipse Public License
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * JSON License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ * 
+ * The Software shall be used for Good, not Evil.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ +++ 
+ * 
+ * MIT
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+package de.uhh.l2g.plugins.util;
+
+import java.util.Date;
+import java.util.List;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+
+import de.uhh.l2g.plugins.NoSuchOaiRecordException;
+import de.uhh.l2g.plugins.model.OaiRecord;
+import de.uhh.l2g.plugins.model.Video;
+import de.uhh.l2g.plugins.model.impl.OaiRecordImpl;
+import de.uhh.l2g.plugins.service.OaiRecordLocalServiceUtil;
+import de.uhh.l2g.plugins.service.VideoLocalServiceUtil;
+
+/**
+ * The OaiPmhManager handles the OAI-PMH-repository
+ *
+ */
+public class OaiPmhManager {
+
+	public OaiPmhManager() {
+		
+	}
+	
+	/**
+	 * Publishes a video to the OAI-PMH-Repository
+	 * 
+	 * If an OaiRecord already exists for the video, it will be updated 
+	 * @param videoId the id of the video which will be published to the OAI-PMH-repository 
+	 */
+	public static void publish(Long videoId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		// check if an OaiRecord already exists for this videoId
+		try {
+			OaiRecord oaiRecord = OaiRecordLocalServiceUtil.getByVideo(videoId);
+			// handle already existing 
+			oaiRecord = fillOaiRecord(oaiRecord, videoId, false);
+			OaiRecordLocalServiceUtil.updateOaiRecord(oaiRecord);
+		} catch (NoSuchOaiRecordException e) {
+			// no record with the video id, create one (this is the normal case
+			OaiRecord oaiRecord = new OaiRecordImpl();
+			oaiRecord = fillOaiRecord(oaiRecord, videoId, false);
+			try {
+				OaiRecordLocalServiceUtil.addOaiRecord(oaiRecord);
+			} catch (SystemException e1) {
+				e1.printStackTrace();
+			}
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Unpublishes a video from the OAI-PMH-Repository 
+	 * Sets the "deleted" flag, so those items still remain in the repository
+	 * @param videoId the id of the video which will be unpublished from the OAI-PMH-repository
+	 */
+	public static void unpublish(Long videoId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		try {
+			// check if an OaiRecord even exists for this videoId
+			OaiRecord oaiRecord = OaiRecordLocalServiceUtil.getByVideo(videoId);
+			oaiRecord = fillOaiRecord(oaiRecord, videoId, true);
+			OaiRecordLocalServiceUtil.updateOaiRecord(oaiRecord);
+		} catch (NoSuchOaiRecordException e) {
+			// no record with the video id, unpublishing not necessary
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Modifies a OaiRecord 
+	 * This will update the datestamp of OAIRecord (If no OAI exists for this video, it will do nothing) 
+	 * @param videoId the id of the video whose datestamp will be modified
+	 */
+	public static void modify(Long videoId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		try {
+			// check if an OaiRecord even exists for this videoId
+			OaiRecord oaiRecord = OaiRecordLocalServiceUtil.getByVideo(videoId);
+			// we only need to update the datestamp to the modified time and only if the record is not deleted
+			if (!oaiRecord.getDeleted()) {
+				oaiRecord.setDatestamp(new Date());
+				OaiRecordLocalServiceUtil.updateOaiRecord(oaiRecord);
+			}
+		} catch (NoSuchOaiRecordException e) {
+			// no record with the video id, modifying not necessary
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Modifies all OaiRecords which are linked to the given creator
+	 * This should be called when a creator name is changed, so all OaiRecords will be updated accordingly
+	 * @param creatorId the creatorId
+	 */
+	public static void modifyByCreator(Long creatorId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		List<OaiRecord> oaiRecords;
+		try {
+			oaiRecords = OaiRecordLocalServiceUtil.getByCreator(creatorId);
+			for (OaiRecord oaiRecord: oaiRecords) {
+				modify(oaiRecord.getVideoId());
+			}
+		} catch (NoSuchOaiRecordException e) {
+			// no OaiRecords for this creator, nothing needs to be modified
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Modifies all OaiRecords which are linked to the given category
+	 * This should be called when a category name is changed, so all OaiRecords will be updated accordingly
+	 * @param categoryId the categoryId
+	 */
+	public static void modifyByCategory(Long categoryId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		List<OaiRecord> oaiRecords;
+		try {
+			oaiRecords = OaiRecordLocalServiceUtil.getByCategory(categoryId);
+			for (OaiRecord oaiRecord: oaiRecords) {
+				modify(oaiRecord.getVideoId());
+			}
+		} catch (NoSuchOaiRecordException e) {
+			// no OaiRecords for this category, nothing needs to be modified
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Modifies all OaiRecords which are linked to the given lectureseries
+	 * This should be called when a lectureseries was changed, so all OaiRecords will be updated accordingly
+	 * @param lectureseriesId the lectureseriesId
+	 */
+	public static void modifyByLectureseries(Long lectureseriesId) {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		List<OaiRecord> oaiRecords;
+		try {
+			oaiRecords = OaiRecordLocalServiceUtil.getByLectureseries(lectureseriesId);
+			for (OaiRecord oaiRecord: oaiRecords) {
+				modify(oaiRecord.getVideoId());
+			}
+		} catch (NoSuchOaiRecordException e) {
+			// no OaiRecords for this category, nothing needs to be modified
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// a modification on insitution basis may not be relevant to the oai-pmh-repository, so this is not implemented for now
+	public static void modifyByInstitution(Long InstitutionId) {
+		
+	}
+	
+	/**
+	 * This publishes all open access video to the OAI-PMH-repository
+	 */
+	public static void publishAllOpenAccessVideos() {
+		if (!checkOaiPmhServiceAllowed()) {
+			return;
+		}
+		try {
+			List<Video> videos = VideoLocalServiceUtil.getByOpenAccess(1);
+			for (Video video: videos) {
+				publish(video.getVideoId());
+			}
+		} catch (SystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Fills the OaiRecord with the given data
+	 * @param oaiRecord the OaiRecord to fill
+	 * @param videoId the corresponding videoId
+	 * @param deleted flag if the record should be marked as deleted
+	 * @return the filled OaiRecord
+	 */
+	private static OaiRecord fillOaiRecord(OaiRecord oaiRecord, Long videoId, boolean deleted) {
+		oaiRecord.setVideoId(videoId);
+		String scheme = PropsUtil.get("lecture2go.oaipmh.identifierscheme");
+		String identifierPrefix = PropsUtil.get("lecture2go.oaipmh.identifierprefix");
+		String delimiter = PropsUtil.get("lecture2go.oaipmh.identifierdelimiter");
+		oaiRecord.setIdentifier(scheme + delimiter + identifierPrefix + delimiter + videoId);
+		oaiRecord.setDatestamp(DateUtil.newDate());
+		oaiRecord.setDeleted(deleted);
+		return oaiRecord;
+	}
+	
+	/**
+	 * Checks if the oai-pmh data updating is allowed via config or not
+	 * @return true if service is allowed, false if not
+	 */
+	private static boolean checkOaiPmhServiceAllowed() {
+		return Boolean.parseBoolean(PropsUtil.get("lecture2go.oaipmh.active"));
+	}
+}
